79fa0902e1d300c793aa192a3a1b40caf273b76a
theabdel572 <abdelkarimachbaniabid572@gmail.com>
2021-01-03 22:22:06 +0100

    Main class started

---
+++ theabdel572/CPSChecker/CPSChecker.java
@@ -0,0 +1,14 @@
+package theabdel572.CPSChecker;
+
+import javax.swing.JFrame;
+
+public class CPSChecker {
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setTitle("CPSChecker - by theabdel572");
+		frame.setVisible(true);
+		frame.setSize(300, 300);
+	}
+}
