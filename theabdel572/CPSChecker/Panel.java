7d7745b71dffa0ad3832fb76b6fb542d9826e16c
theabdel572 <abdelkarimachbaniabid572@gmail.com>
2021-01-05 16:26:44 +0100

    Added CPS Counter

--- theabdel572/CPSChecker/Panel.java
+++ theabdel572/CPSChecker/Panel.java
@@ -1,5 +1,7 @@
 package theabdel572.CPSChecker;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,12 +10,15 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Panel extends JPanel{
+	Font font = new Font("CPSFont", 1, 16);
 	JButton click = new JButton("       CLICK HERE!!!       ");
 	String i = "0";
 	int CPS = Integer.parseInt(i);
-	String x = String.valueOf(CPS);
 	public void paintComponent(Graphics g) {
-		g.drawString(i, 135, 135);
+		g.setFont(font);
+		g.drawRect(70, 100, 150, 25);
+		g.setColor(Color.white);
+		g.drawString(i, 75, 118);
 	}
 	public Panel() {
 		add(click);
@@ -21,9 +26,16 @@ public class Panel extends JPanel{
 		click.addActionListener(onClick);
 	}
 	private class Listener implements ActionListener{
+		
+		
 		public void actionPerformed(ActionEvent e) {
+			Graphics g = getGraphics();
 			CPS++;
-			System.out.println(CPS);
+			g.fillRect(70, 100, 150, 25);
+			String x = String.valueOf(CPS);
+			g.setColor(Color.white);
+			g.setFont(font);
+			g.drawString(x, 75, 118);
 		}
 	}
 }
